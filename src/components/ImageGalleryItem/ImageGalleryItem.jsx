e11089d400c0421299e8f20fe31ba3a661bd830b
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-07-08 19:06:27 +0300

    refactoring ImageGalleryItem c-t int function and create useToggle hook

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,31 +1,22 @@
-import React, { Component } from 'react'
+import { useToggle } from '../../hooks'
 import Modal from '../Modal'
 import { Item, Image } from './ImageGalleryItem.styled'
 
-export default class ImageGalleryItem extends Component {
-  state = {
-    showModal: false,
-  }
+export default function ImageGalleryItem({
+  item: { webformatURL, largeImageURL, tags },
+}) {
+  const [showModal, toggleModal] = useToggle()
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }))
-  }
-
-  render() {
-    const { webformatURL, largeImageURL, tags } = this.props.item
-    const { showModal } = this.state
-
-    return (
-      <>
-        <Item>
-          <Image onClick={this.toggleModal} src={webformatURL} alt={tags} />
-        </Item>
-        {showModal && (
-          <Modal onClose={this.toggleModal}>
-            <img src={largeImageURL} alt={tags} />
-          </Modal>
-        )}
-      </>
-    )
-  }
+  return (
+    <>
+      <Item>
+        <Image onClick={toggleModal} src={webformatURL} alt={tags} />
+      </Item>
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <img src={largeImageURL} alt={tags} />
+        </Modal>
+      )}
+    </>
+  )
 }
